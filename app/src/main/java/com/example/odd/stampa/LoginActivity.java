0dae2eb80e077578768c38880f9f78b86ecc1c02
vrjulianti <virariv07@gmail.com>
2018-05-18 14:25:23 +0700

    first_commit

---
+++ app/src/main/java/com/example/odd/stampa/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.odd.stampa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
